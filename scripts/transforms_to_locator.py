7e04152d20b74ec32dbb8c3469dbc955d8d01c05
Dmytro Holub <heap3d@gmail.com>
2024-10-30 00:45:15 +0200

    added: transforms to locators functionality

--- scripts/transforms_to_locator.py
+++ scripts/transforms_to_locator.py
@@ -15,18 +15,20 @@ import lx
 from typing import Union
 
 from h3d_utilites.scripts.h3d_debug import H3dDebug
-from h3d_utilites.scripts.h3d_utils import get_parent_index
+from h3d_utilites.scripts.h3d_utils import get_parent_index, replace_file_ext
 import h3d_meshref_hierarchy_setup.scripts.h3d_kit_constants as h3dc
 
 
 LOCATOR_SUFFIX = ' loc'
+ARG_SELECTED = 'selected'
+ARG_FORCED_SELECTED = 'forced'
 
 
 def get_transforms(item: modo.Item) -> tuple[
-        tuple[float, float, float],
-        tuple[float, float, float],
-        tuple[float, float, float],
-        ]:
+    tuple[float, float, float],
+    tuple[float, float, float],
+    tuple[float, float, float],
+]:
     px = lx.eval(f'transform.channel pos.X ? item:{item.id}')
     py = lx.eval(f'transform.channel pos.Y ? item:{item.id}')
     pz = lx.eval(f'transform.channel pos.Z ? item:{item.id}')
@@ -42,22 +44,39 @@ def get_transforms(item: modo.Item) -> tuple[
 
 def is_nonzero_transforms(item: modo.Item) -> bool:
     pos, rot, scl = get_transforms(item)
+
     if any((*pos, *rot)):
         return True
+
     if scl[0] != 1.0 or scl[1] != 1.0 or scl[2] != 1.0:
         return True
+
     return False
 
 
 def is_transforms_matched(
-        transform1: tuple[tuple[float, float, float], tuple[float, float, float], tuple[float, float, float]],
-        transform2: tuple[tuple[float, float, float], tuple[float, float, float], tuple[float, float, float]],
-        precision: int = 3,
-        ) -> bool:
+    transform1: tuple[
+        tuple[float, float, float],
+        tuple[float, float, float],
+        tuple[float, float, float],
+    ],
+    transform2: tuple[
+        tuple[float, float, float],
+        tuple[float, float, float],
+        tuple[float, float, float],
+    ],
+    precision: int = 3,
+) -> bool:
     flat1 = [val for transform in transform1 for val in transform]
     flat2 = [val for transform in transform2 for val in transform]
-    if not all([round(val[0], precision) == round(val[1], precision) for val in zip(flat1, flat2)]):
+    if not all(
+        [
+            round(val[0], precision) == round(val[1], precision)
+            for val in zip(flat1, flat2)
+        ]
+    ):
         return False
+
     return True
 
 
@@ -65,6 +84,7 @@ def matched_item(item: modo.Item, items: list[modo.Item]) -> Union[modo.Item, No
     item_transforms = get_transforms(item)
     for itemto in items:
         itemto_transforms = get_transforms(itemto)
+
         if is_transforms_matched(item_transforms, itemto_transforms):
             return itemto
 
@@ -81,53 +101,172 @@ def parent(item: modo.Item, parent: Union[modo.Item, None] = None, inplace: int 
     if not parent:
         lx.eval(f'item.parent item:{item.id} parent:{{}} position:{position} inPlace:{inplace}')
         return
+
     lx.eval(f'item.parent item:{item.id} parent:{parent.id} position:{position} inPlace:{inplace}')
 
 
 def is_hierarchy_setup_item(item: modo.Item) -> bool:
     if str(item.name).startswith(h3dc.MESH_PREFIX):
         return True
+
     if str(item.name).startswith(h3dc.ROOT_PREFIX):
         return True
+
     return False
 
 
 def convert_transforms(meshes: list[modo.Mesh], locators: list[modo.Item]) -> list[modo.Item]:
     working_locators = locators[:]
+
     for mesh in meshes:
-        printd(f'{mesh.name}, {mesh=}')
         if is_hierarchy_setup_item(mesh):
-            printd('is hierarchy setup item. skipped.', 1)
             continue
+
         if not is_nonzero_transforms(mesh):
-            printd('not is_nonzero_transfers. skipped.', 1)
             continue
-        printd(f'non-zero transforms detected. {mesh.name=} {mesh=}', 1)
+
         locator = matched_item(mesh, working_locators)
         if not locator:
-            printd("locator doesn't exist. creating...", 1)
             locator = modo.Scene().addItem(itype=c.LOCATOR_TYPE)
             locator.name = mesh.name + LOCATOR_SUFFIX
             match_item(locator, mesh)
             parent(locator, position=get_parent_index(mesh))
             working_locators.append(locator)
-        else:
-            printd('locator found', 1)
-        printd(f'locator: {locator.name} {locator=}', 1)
+
         parent(mesh, locator)
 
     return working_locators[len(locators):]
 
 
+def convert_transforms_forced(meshes: list[modo.Mesh], locators: list[modo.Item]) -> list[modo.Item]:
+    working_locators = locators[:]
+
+    for mesh in meshes:
+        if is_hierarchy_setup_item(mesh):
+            continue
+
+        locator = matched_item(mesh, working_locators)
+        if not locator:
+            locator = modo.Scene().addItem(itype=c.LOCATOR_TYPE)
+            locator.name = mesh.name + LOCATOR_SUFFIX
+            match_item(locator, mesh)
+            parent(locator, mesh.parent, position=get_parent_index(mesh))
+            working_locators.append(locator)
+
+        parent(mesh, locator)
+
+    return working_locators[len(locators):]
+
+
+def get_meshes():
+    meshes = [
+        item
+        for item in modo.Scene().meshes
+        if not item.parent
+    ]
+
+    mesh_instances = [
+        item
+        for item in modo.Scene().items(itype=c.MESHINST_TYPE)
+        if not item.parent
+    ]
+
+    meshes.extend(mesh_instances)  # type: ignore
+
+    return meshes
+
+
+def get_selected_meshes():
+    meshes = [
+        item
+        for item in modo.Scene().selectedByType(itype=c.MESH_TYPE)
+        if not item.parent
+    ]
+
+    mesh_instances = [
+        item
+        for item in modo.Scene().selectedByType(itype=c.MESHINST_TYPE)
+        if not item.parent
+    ]
+
+    meshes.extend(mesh_instances)  # type: ignore
+
+    return meshes
+
+
+def get_forced_selected_meshes():
+    meshes = [
+        item
+        for item in modo.Scene().selectedByType(itype=c.MESH_TYPE)
+    ]
+
+    mesh_instances = [
+        item
+        for item in modo.Scene().selectedByType(itype=c.MESHINST_TYPE)
+    ]
+
+    meshes.extend(mesh_instances)  # type: ignore
+
+    return meshes
+
+
+def get_locators():
+    locators = [
+        item
+        for item in modo.Scene().items(itype=c.LOCATOR_TYPE, superType=False)
+        if not item.parent
+    ]
+
+    return locators
+
+
+def get_selected_locators():
+    locators = [
+        item
+        for item in modo.Scene().selectedByType(itype=c.LOCATOR_TYPE, superType=False)
+    ]
+
+    return locators
+
+
+def default_action() -> tuple[modo.Item, ...]:
+    meshes = get_meshes()
+    locators = get_locators()
+    new_locators = convert_transforms(meshes, locators)
+
+    return tuple(new_locators)
+
+
+def selected_action() -> tuple[modo.Item, ...]:
+    meshes = get_selected_meshes()
+    locators = get_locators()
+    new_locators = convert_transforms(meshes, locators)
+
+    return tuple(new_locators)
+
+
+def forced_selected_action() -> tuple[modo.Item, ...]:
+    meshes = get_forced_selected_meshes()
+    locators = get_selected_locators()
+    new_locators = convert_transforms_forced(meshes, locators)
+
+    return tuple(new_locators)
+
+
 def main():
-    meshes = [item for item in modo.Scene().meshes if not item.parent]
-    printi(meshes, 'meshes:')
-    locators = [item for item in modo.Scene().items(itype=c.LOCATOR_TYPE, superType=False) if not item.parent]
-    printi(locators, 'locators:')
+    try:
+        arg = lx.args()[0]  # type: ignore
+    except IndexError:
+        arg = ''
+
+    actions = {
+        ARG_SELECTED: selected_action,
+        ARG_FORCED_SELECTED: forced_selected_action,
+    }
 
     selected = modo.Scene().selected
-
-    new_locators = convert_transforms(meshes, locators)
+    action = actions.get(arg, default_action)
+    new_locators = action()
 
     modo.Scene().deselect()
     if new_locators:
@@ -138,8 +277,9 @@ def main():
             item.select()
 
 
+h3dd = H3dDebug(enable=False, file=replace_file_ext(modo.Scene().name, '.log'))
+printd = h3dd.print_debug
+printi = h3dd.print_items
+
 if __name__ == '__main__':
-    h3dd = H3dDebug(enable=False, file=modo.Scene().name + '.log')
-    printd = h3dd.print_debug
-    printi = h3dd.print_items
     main()
