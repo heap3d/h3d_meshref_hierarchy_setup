d9a70ae1c320bfeac10dda23f5807e984f71b4bc
Dmytro Holub <heap3d@gmail.com>
2024-11-07 20:40:26 +0200

    added: align new locator to position and rotation modified: align to first selected item

--- scripts/parent_to_new_loc.py
+++ scripts/parent_to_new_loc.py
@@ -12,7 +12,7 @@ from typing import Iterable
 import modo
 import modo.constants as c
 
-from h3d_utilites.scripts.h3d_utils import parent_items_to, get_parent_index
+from h3d_utilites.scripts.h3d_utils import parent_items_to, get_parent_index, match_pos_rot
 
 
 PARENT_LOC_SFX = 'loc'
@@ -20,7 +20,11 @@ PARENT_LOC_SFX = 'loc'
 
 def main():
     selected = modo.Scene().selectedByType(itype=c.LOCATOR_TYPE, superType=True)
-    parent_align_item = selected[-1]
+    if not selected:
+        return
+
+    parent_align_item = selected[0]
+
     parent_loc = create_parent(selected, parent_align_item)
     parent_loc.select(replace=True)
 
@@ -28,6 +32,7 @@ def main():
 def create_parent(items: Iterable[modo.Item], head_item: modo.Item) -> modo.Item:
     parent_loc_name = f'{head_item.name} {PARENT_LOC_SFX}'
     parent_loc = modo.Scene().addItem(itype=c.LOCATOR_TYPE, name=parent_loc_name)
+    match_pos_rot(parent_loc, head_item)
 
     for item in items:
         parent_items_to([item,], parent_loc, get_parent_index(item))
